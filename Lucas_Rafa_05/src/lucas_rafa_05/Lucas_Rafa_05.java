30341d03123e2d46611c68ec20775c7bdb8c0cd4
Lucas Daniel da Costa Rabaquim <98958822+LucasRabaquim@users.noreply.github.com>
2022-03-25 10:40:12 -0300

    Upload da atividade 5

---
+++ Lucas_Rafa_05/src/lucas_rafa_05/Lucas_Rafa_05.java
@@ -0,0 +1,11 @@
+package lucas_rafa_05;
+import java.util.Scanner;
+public class Lucas_Rafa_05 {
+    public static void main(String[] args) {
+       Scanner entrada = new Scanner(System.in);
+       System.out.print("Digite seu horário de entrada: ");
+       double hora = entrada.nextDouble() + 6;
+       hora = (hora >= 24)?(hora - 24):hora;
+       System.out.print("O horário de saida é: " + hora +"h" );
+    }   
+}
